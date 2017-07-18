1ad01182e50542705bd434d95ede3f46da654513
caroquintana <caro.quintana.n@gmail.com>
2017-07-18 13:54:09 -0400

    fin

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	$('i.fa.fa-microphone').click( function(e){
+		e.preventDefault();
+        var microphone = $('input.mensajechat').val();
+        localStorage.setItem("microphone", microphone);
+        $('div.cajaderechados').append('<div class="textoventana"><p>' + localStorage.getItem("microphone") + '</p>');
+        $('input.mensajechat').val('');
+    });
+
+    function addTodoItem(e) {
+    	if(e.type==="keydown" && e.which !== 13) return;
+    		e.preventDefault();
+    		var microphone = $('input.mensajechat').val();
+    	$('div.cajaderechados').append('<div class="textoventana"><p>' + localStorage.getItem("microphone") + '</p>');
+        $('input.mensajechat').val('');
+    
+  }
+
+  $("input.mensajechat").keydown(addTodoItem);
+
+});
+
